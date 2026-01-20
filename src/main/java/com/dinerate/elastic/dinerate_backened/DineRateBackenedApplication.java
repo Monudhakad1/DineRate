6f3f5ca597ff7d00c2743ada3c9405bacc4b352b
Monudhakad1 <monudhakad055@gmail.com>
2026-01-20 23:26:25 +0530

    added .io ready

---
+++ src/main/java/com/dinerate/elastic/dinerate_backened/DineRateBackenedApplication.java
@@ -0,0 +1,13 @@
+package com.dinerate.elastic.dinerate_backened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineRateBackenedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DineRateBackenedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinerate/elastic/dinerate_backened/DineRateBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinerate.elastic.dinerate_backened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineRateBackenedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
