e13422f6e6aee9cce1f3182ef7dd8f51219ea9c2
Monudhakad1 <monudhakad055@gmail.com>
2026-01-24 02:01:34 +0530

    Entites 4 done

--- src/main/java/domains/entities/TimeRanges.java
+++ src/main/java/domains/entities/TimeRanges.java
@@ -1,4 +1,17 @@
 package domains.entities;
 
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
 public class TimeRanges {
+
+    @Field(type= FieldType.Keyword)
+    private String openTime;
+
+    @Field(type= FieldType.Keyword)
+    private String closeTime;
+
 }
